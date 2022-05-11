f573c4413faf9b7469bd73b10dd312b28966f2e9
Joshua Hegedus <jhegedus9@gmail.com>
2022-05-11 21:00:58 +0200

    Proj 12 Done

---
+++ Unity/Unity_12/Assets/Scripts/CoinRotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotate : MonoBehaviour
+{
+    public float delta = 0.5f;
+    public float speed = 3.0f;
+    Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed, 0, 0);
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}

39341513f9287ccaf1b3063e91adcfd4e9f65955
Joshua Hegedus <jhegedus9@gmail.com>
2022-05-25 11:45:21 +0200

    14 finish

--- Unity/Unity_14-15/Assets/Scripts/BallControl.cs
+++ Unity/Unity_14-15/Assets/Scripts/BallControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,28 +8,38 @@ public class BallControl : MonoBehaviour
     public float Movement = 20f;
     public float JumpForce = 300f;
     public bool CanJump = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(0, 0, Movement * Time.deltaTime);
+        transform.Translate(0, 0, Movement * Time.deltaTime);
 
         if (Input.GetKey("a"))
         {
-            this.transform.Translate(-Movement * Time.deltaTime, 0, 0);
+            transform.Translate(-5f * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("d"))
         {
-            this.transform.Translate(Movement * Time.deltaTime, 0, 0);
+            transform.Translate(5f * Time.deltaTime, 0, 0);
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && CanJump)
         {
-            
+            CanJump = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpForce, 0));
+        }
+        if (transform.position.y < -5)
+        {
+            transform.position = new Vector3(0, 1.5f, 5);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CanJump = true;
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            transform.position = new Vector3(0, 1.5f, 5);
         }
     }
 }

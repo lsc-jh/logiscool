26a94824a7d59b20288e6caf66192a240378a663
Joshua Hegedus <jhegedus9@gmail.com>
2023-02-04 16:04:50 +0100

    16. class

--- Unity_2/U2_14-17-VR/Assets/Scripts/FlyingControls.cs
+++ Unity_2/U2_14-17-VR/Assets/Scripts/FlyingControls.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -12,11 +14,21 @@ public class FlyingControls : MonoBehaviour
 
     public Image blackScreen;
     public Animator animator;
-    
+
+    public Transform CheckpointsObject;
+    private List<Transform> checkpoints = new List<Transform>();
+    public Text WinText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        WinText.enabled = false;
+        for (var i = 0; i < CheckpointsObject.childCount; i++)
+        {
+            checkpoints.Add(CheckpointsObject.GetChild(i));
+            checkpoints[i].gameObject.SetActive(false);
+        }
+        checkpoints[0].gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -24,6 +36,24 @@ public class FlyingControls : MonoBehaviour
     {
         speed = baseSpeed + GvrVRHelpers.GetHeadRotation().x * 10;
         transform.position = Vector3.MoveTowards(transform.position, transform.position + GvrVRHelpers.GetHeadForward(), speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == checkpoints[0])
+        {
+            checkpoints.RemoveAt(0);
+            Destroy(other.gameObject);
+            if (checkpoints.Count == 0)
+            {
+                Debug.Log("Win");
+                StartCoroutine(Win());
+            }
+            else
+            {
+                checkpoints[0].gameObject.SetActive(true);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,5 +67,13 @@ public class FlyingControls : MonoBehaviour
         animator.SetBool("Fade", true);
         yield return new WaitUntil(() => blackScreen.color.a == 1);
         SceneManager.LoadScene("SampleScene");
+    }
+
+    IEnumerator Win()
+    {
+        WinText.enabled = true;
+        yield return new WaitForSeconds(3);
+        WinText.enabled = false;
+        StartCoroutine(Fading());
     }
 }
